3c2b514bc0c11d138e61b4a226edb9e82718c308
Mustafa Topal2 <cams@Mustafa-MacBook-Pro.local>
2019-05-16 23:33:17 +0300

    init

---
+++ data11.py
@@ -0,0 +1,80 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 13 20:09:05 2019
+
+@author: tayfu
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 13 03:29:31 2019
+
+@author: tayfu
+"""
+
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 15 04:18:20 2018
+"""
+
+#1. kutuphaneler
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import statsmodels.api as sm
+#2. Veri Onisleme
+
+#2.1. Veri Yukleme
+datas = pd.read_csv('data1.csv')
+#pd.read_csv("veriler.csv")
+
+from sklearn.model_selection import train_test_split
+
+#verilerin egitim ve test icin bolunmesi
+
+
+
+x_train, x_test,y_train,y_test = train_test_split(datas.iloc[:,:-1],datas.iloc[:,-1:],test_size=0.33, random_state=0)
+
+
+
+
+from sklearn.tree import DecisionTreeRegressor
+regressor = DecisionTreeRegressor(random_state=0)
+regressor.fit(x_train,y_train)
+
+y_pred1 = regressor.predict(x_test)
+
+print(y_pred1)
+
+katilimci =  [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30]
+
+
+
+x_train = x_train.iloc[:,1:]
+x_test = x_test.iloc[:,1:]
+
+
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(x_train,y_train)
+y_pred = regressor.predict(x_test)
+
+from sklearn.svm import SVR
+regressor = SVR(kernel='rbf')
+regressor.fit(x_train,y_train)
+y_pred2 = regressor.predict(x_test)
+
+
+
+plt.title("pred dec.tree(green), pred mul.lin.reg.(blue) support vector reg (yellow) vs real (red)")
+plt.xlabel("katilimci")
+plt.ylabel("score (tons)")
+plt.plot(katilimci,y_pred1, color='green')
+plt.plot(katilimci,y_pred2, color='yellow')
+plt.plot(katilimci,y_pred, color='blue')
+plt.plot(katilimci,y_test, color='red')
+
+plt.show()
+
